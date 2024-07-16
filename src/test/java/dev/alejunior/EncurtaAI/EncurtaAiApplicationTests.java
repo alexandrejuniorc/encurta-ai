072baefbbfdb686b9a78f8668c4db8880ce1b0dc
Alexandre Junior <alejunior.dev@gmail.com>
2024-07-15 23:28:33 -0300

    feat: create project

---
+++ src/test/java/dev/alejunior/EncurtaAI/EncurtaAiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.alejunior.EncurtaAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncurtaAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
